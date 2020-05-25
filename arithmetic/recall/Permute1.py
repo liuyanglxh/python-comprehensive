b454b3b3bd97e98f8d1023d9910ac1790aa8f41e
liuyang <yang.liu@mail.dealmoon.com>
2020-05-25 17:34:33 +0800

    1

--- arithmetic/recall/Permute1.py
+++ arithmetic/recall/Permute1.py
@@ -11,33 +11,39 @@ class Solution:
         self.cache: dict[str: List[int]] = {}
 
     def permute(self, nums: List[int]) -> List[List[int]]:
+        return self.permute_sub(sorted(nums))
+
+    def permute_sub(self, nums: List[int]) -> List[List[int]]:
         if len(nums) == 1 or len(nums) == 0:
             return [nums[:]]
 
-        result: List[List[int]]
-
         # 避免重复计算的缓存
-        sorted(nums)
         key: str = nums.__str__()
         if key in self.cache:
-            result = self.cache[key]
-        else:
-            result = []
-            for index in range(0, len(nums)):
-                num = nums[index]
-                sub_nums = nums[:]
-                sub_nums.remove(num)
+            return self.cache[key]
 
-                sub_list: List[List[int]] = self.permute(sub_nums)
-                for sub_index, sub_result in enumerate(sub_list):
-                    head: List[int] = [num] * (1 + len(sub_result))
-                    for i, e in enumerate(sub_result):
-                        head[i + 1] = e
-                    result.append(head)
+        total = 1
+        for t in range(1, len(nums) + 1):
+            total *= t
 
-            self.cache[key] = result
+        result: List[List[int]] = [[]] * total
+        result_index = 0
+        for index in range(0, len(nums)):
+            num = nums[index]
+            sub_nums = nums[:]
+            sub_nums.remove(num)
+
+            sub_list: List[List[int]] = self.permute_sub(sub_nums)
+            for sub_index, sub_result in enumerate(sub_list):
+                head: List[int] = [num] * (1 + len(sub_result))
+                for i, e in enumerate(sub_result):
+                    head[i + 1] = e
+                result[result_index] = head
+                result_index += 1
+
+        self.cache[key] = result
 
         return result
 
 
-print(Solution().permute([1, 2, 3, 4]))
+print(Solution().permute([4, 3, 2, 1]))
