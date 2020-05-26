1882c803a8fefde0e9f1456f7fb9ce909d847a84
liuyang <yang.liu@mail.dealmoon.com>
2020-05-26 11:00:35 +0800

    1

--- basic/Str.py
+++ basic/Str.py
@@ -1,2 +1,16 @@
 name = 'jk'
 print(f'my name is {name}')
+
+s = "abc"
+s1 = s.capitalize()
+print(s)
+print(s1)
+
+name = 'abc'
+print(name.center(20, "*"))
+ss = "0123456789"
+print(ss[0:len(ss):2])
+
+print("abc".upper())
+print("ABC".lower())
+print("this is a game".title().swapcase())
